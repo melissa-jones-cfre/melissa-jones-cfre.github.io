ee0116a2e7ba9961be884d5fb46647eed5e09cb7
Christian Jensen <devjensen91@gmail.com>
2020-02-15 00:00:00 -0600

    added to top button

--- melissa-jones-cfre-github-io/src/app/app.component.ts
+++ melissa-jones-cfre-github-io/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'melissa-jones-cfre-github-io';
+  @HostListener('window:scroll', []) onWindowScroll() {
+    this.scrollFunction();
+  }
+
+  scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById('myBtn').style.display = 'block';
+    } else {
+      document.getElementById('myBtn').style.display = 'none';
+    }
+  }
+
+  topFunction() {
+    document.body.scrollTo({ top: 0, behavior: 'smooth' }); // For Safari
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
+  }
 }
